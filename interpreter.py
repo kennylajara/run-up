66d5bd27814dff4aa74c38d0ae3fd5850a2a9c36
Kenny Lajara <kennylajara@gmail.com>
2021-06-05 11:35:03 -0400

    Implemented Interpreter_1:set_environment

--- interpreter.py
+++ interpreter.py
@@ -1,24 +1,96 @@
+# Built-in
 from abc import ABC, abstractmethod
+import os
+from pathlib import Path
+
+# 3rd party
+import click
+
+# Own
+from runupdb import RunupDB
 
 
 class Interpreter(ABC):
     """Interpreters' abstract class."""
+
+    @abstractmethod
+    def __init__(self, context:Path, verbose:bool, version:str) -> None:
+        """Set interpreter variables."""
+        self._context:Path = context
+        self._verbose:bool = verbose
+        self._version:str = version
         
     @abstractmethod
-    def create_backup(self):
+    def create_backup(self) -> bool:
+        """Create a new backup."""
         pass
         
     @abstractmethod
-    def restore_backup(self):
+    def restore_backup(self, backup_id:str) -> bool:
+        """Restore the specified backup."""
+        pass
+
+    @abstractmethod
+    def set_environment(self) -> bool:
+        """Create the backup enviroment."""
         pass
 
 
 class Interpreter_1(Interpreter):
     """Interpreter that implements the rules for YAML version 1."""
     
-    def create_backup(self):
+    def __init__(self, context:Path, verbose:bool):
+        super(Interpreter_1, self).__init__(
+            context=context,
+            verbose=verbose,
+            version='1',
+        )
+
+    def create_backup(self) -> bool:
         pass
 
-    def restore_backup(self):
+    def restore_backup(self, backup_id:str) -> bool:
         pass
-    
+
+    def set_environment(self) -> bool:
+        """
+        Create the backup enviroment.
+
+        Creates a directory `.runup` at context level. In it
+        creates a `.version` that only contains a number `1`
+        and creates a SQLite database named `runup.db`.
+        """
+
+        if self._verbose:
+            click.echo('Setting environment.')
+
+        # Create the directory `.runup`
+        if not os.path.exists(f'{self._context}/.runup'):
+            os.mkdir(f'{self._context}/.runup')
+            if self._verbose:
+                click.echo(f'Created directory {self._context}/.runup')
+        else:
+            if self._verbose:
+                click.echo(f'The directory `{self._context}/.runup` already exists.')
+            click.echo('RunUp is already initiated.')
+            return False
+        
+        # Create file `.version`
+        if not os.path.exists(f'{self._context}/.runup/.version'):
+            with open(f'{self._context}/.runup/.version', "w") as file:
+                file.write(self._version)
+            if self._verbose:
+                click.echo(f'created file {self._context}/.runup/.version')
+        else:
+            if self._verbose:
+                click.echo(f'The file `{self._context}/.runup/.version` already exists.')
+            click.echo('RunUp is already initiated.')
+            return False
+
+        # Create database
+        RunupDB(self._context, self._verbose).create_database()
+
+        if self._verbose:
+            click.echo('-'*10)
+
+        return True
--- tests/ParserYAML/test_runup.py
+++ tests/ParserYAML/test_runup.py
@@ -1,3 +1,5 @@
+# Built-in
+from pathlib import Path
 from typing import (
     Any,
     Dict,
@@ -5,39 +7,52 @@ from typing import (
 import unittest
 from unittest import mock
 
+# 3rd party
 import yaml
 
-from runup import RunupYAML
+# Own
+from runup import ParserYAML
 from utils.runup import get_yaml_latest_version
 
 
-class TestRunupYAML(unittest.TestCase):
+class TestParserYAML(unittest.TestCase):
 
     @mock.patch('runup.click.echo', return_value=None)
     def test__read_yaml_file(self, muck_click_echo):
         """Find a `runup.yml` in the given context."""
-        
+
+        # Type hint
+        corrupted_file:None
+        no_file:None
+        path:Path
+        yaml_file:Dict
+        yml_file:Dict
+
         # Search for runup.yaml
-        yaml_file:Dict = RunupYAML('.')._read_yaml_file('./tests/RunupYaml/read/yaml')
+        path, yaml_file = ParserYAML('.', True)._read_yaml_file('./tests/ParserYAML/read/yaml')
+        self.assertIsInstance(path, Path)
         self.assertIsInstance(yaml_file, Dict)
 
         # Search for runup.yml
-        yml_file:Dict = RunupYAML('.')._read_yaml_file('./tests/RunupYaml/read/yml')
+        path, yml_file = ParserYAML('.', True)._read_yaml_file('./tests/ParserYAML/read/yml')
+        self.assertIsInstance(path, Path)
         self.assertIsInstance(yml_file, Dict)
 
         # Shouldn't find the file
-        no_file:None = RunupYAML('.')._read_yaml_file('./tests/RunupYaml/read/none')
+        path, no_file = ParserYAML('.', True)._read_yaml_file('./tests/ParserYAML/read/none')
+        self.assertIsInstance(path, Path)
         self.assertIsNone(no_file)
 
         # Shouldn't find the file
-        corrupted_file:None = RunupYAML('.')._read_yaml_file('./tests/RunupYaml/read/corrupted')
+        path, corrupted_file = ParserYAML('.', True)._read_yaml_file('./tests/ParserYAML/read/corrupted')
+        self.assertIsInstance(path, Path)
         self.assertIsNone(corrupted_file)
 
     @mock.patch('runup.click.echo', return_value=None)
     def test__get_version(self, muck_click_echo):
         """Read the version of the a YAML file"""
 
-        context:str = './tests/RunupYaml/version'
+        context:str = './tests/ParserYAML/version'
         expected_values:Dict[str, Any] = {
             'missing-version': None,
             'unsupported-version': None,
@@ -48,6 +63,6 @@ class TestRunupYAML(unittest.TestCase):
         for filename, expected_value in expected_values.items():
             with open(f'{context}/{filename}.yaml') as stream:
                 yaml_content:dict = yaml.safe_load(stream)
-                real_value = RunupYAML('.')._get_version(yaml_content)
+                real_value = ParserYAML('.', True)._get_version(yaml_content)
 
             self.assertEqual(expected_value, real_value)
